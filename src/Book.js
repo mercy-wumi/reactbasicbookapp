84d97940be672fa6d980af265a03790a5dea02ed
mercy-wumi <m.adams1909@gmail.com>
2022-03-20 01:01:32 +0100

    convert the app into class component

--- src/Book.js
+++ src/Book.js
@@ -1,34 +1,68 @@
 import React from 'react'
 
-const Book = ({ img, title, author }) => {
- // attribute, eventHandler
- // onClick, onMouseOver
- const clickHandler = (e) => {
-  console.log(e);
-  console.log(e.target);
-  alert('hello world');
- };
- const complexExample = (author) => {
-  console.log(author);
- };
- return (
-  <article
-   className='book'
-   onMouseOver={() => {
-    console.log(title);
-   }}
-  >
-   <img src={img} alt='' />
-   <h1 onClick={() => console.log(title)}>{title}</h1>
-   <h4>{author}</h4>
-   <button type='button' onClick={clickHandler}>
-    reference example
-      </button>
-   <button type='button' onClick={() => complexExample(author)}>
-    more complex example
-      </button>
-  </article>
- );
-};
+// const Book = ({ img, title, author }) => {
+//  // attribute, eventHandler
+//  // onClick, onMouseOver
+//  const clickHandler = (e) => {
+//   console.log(e);
+//   console.log(e.target);
+//   alert('hello world');
+//  };
+//  const complexExample = (author) => {
+//   console.log(author);
+//  };
+//  return (
+//   <article
+//    className='book'
+//    onMouseOver={() => {
+//     console.log(title);
+//    }}
+//   >
+//    <img src={img} alt='' />
+//    <h1 onClick={() => console.log(title)}>{title}</h1>
+//    <h4>{author}</h4>
+//    <button type='button' onClick={clickHandler}>
+//     reference example
+//       </button>
+//    <button type='button' onClick={() => complexExample(author)}>
+//     more complex example
+//       </button>
+//   </article>
+//  );
+// };
+
+class Book extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const clickHandler = (e) => {
+      console.log(e);
+      console.log(e.target);
+      alert('hello world');
+    };
+    const complexExample = (author) => {
+      console.log(author);
+    };
+    return (
+      <article
+        className='book'
+        onMouseOver={() => {
+          console.log(this.props.title);
+        }}
+      >
+        <img src={this.props.img} alt='' />
+        <h1 onClick={() => console.log(this.props.title)}>{this.props.title}</h1>
+        <h4>{this.props.author}</h4>
+        <button type='button' onClick={clickHandler}>
+          reference example
+        </button>
+        <button type='button' onClick={() => complexExample(this.props.author)}>
+          more complex example
+        </button>
+      </article>
+    )
+  }
+}
 
 export default Book
--- src/index.js
+++ src/index.js
@@ -7,17 +7,36 @@ import { data } from './books'
 import SpecificBook from './Book'
 import { greeting } from './testing/testing'
 
-function BookList() {
-  console.log(greeting);
-  return (
-    <section className='booklist'>
-      {data.map(book => {
-        return <SpecificBook key={book.id} {...book}></SpecificBook>;
-      })}
-    </section>
-  );
-}
+// function BookList() {
+//   console.log(greeting);
+//   return (
+//     <section className='booklist'>
+//       {data.map(book => {
+//         return <SpecificBook key={book.id} {...book}></SpecificBook>;
+//       })}
+//     </section>
+//   );
+// }
 
+class BookList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bookdata: data
+    }
+  }
+  render() {
+    console.log(greeting);
+
+    return (
+      <section className='booklist'>
+        {this.state.bookdata.map(book => {
+          return <SpecificBook key={book.id} {...book}></SpecificBook>;
+        })}
+      </section>
+    )
+  }
+}
 
 
 ReactDom.render(<BookList />, document.getElementById('root'));
